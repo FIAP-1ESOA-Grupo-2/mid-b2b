b9d2e46996babe0bd5b12c2d89ef3d6a908bfb8a
Lucas Alves Lobo <59810995+LucasAlvesL@users.noreply.github.com>
2024-03-04 18:36:10 -0300

    Fix: SM banner fix size

--- src/components/Auth/Banner.tsx
+++ src/components/Auth/Banner.tsx
@@ -1,15 +1,14 @@
 import banner from '@/assets/images/bannerauth.svg'
 import mobileBanner from '@/assets/images/Illustrationsignup.svg'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
 
 export const Banner = () => {
-    return <div className='container'>
-        <div className='md:hidden'>
+    return <div>
+        <div className='md:hidden sm:hidden'>
             <Image src={mobileBanner} alt='Imagem do banner mobile'/>
         </div>
-        <div className=''>
-            <Image src={banner} alt='Imagem do banner mobile' className='hidden md:block'/>
+        <div className='hidden sm:block'>
+            <Image src={banner} alt='Imagem do banner mobile'/>
         </div>
     </div>
 }
