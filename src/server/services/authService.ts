811db71f2486978be5b71d0f8e648215705ede6d
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-03-05 16:49:30 -0300

    Change: Change Auth components

--- src/server/services/authService.ts
+++ src/server/services/authService.ts
@@ -1,7 +1,7 @@
 "use server"
 
 import { PrismaClient } from '@prisma/client'
-import { User } from '@/types/Auth'
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient()
 
@@ -21,13 +21,32 @@ export const createUser = async (
         }
     };
 
+    let hashedPassword = await bcrypt.hash(password, 10);
+
     await prisma.user.create({
-        data: { name, email, cpf, sector, role, accountType, password, phoneNumber }
+        data: { name, email, cpf, sector, role, accountType, password: hashedPassword, phoneNumber }
     })
 
     return { error: '' }
 }
 
-export const deleteUser = async () => {
+export const checkUser = async (email_or_cpf: string, password: string) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            OR: [
+                { email: email_or_cpf },
+                { cpf: email_or_cpf }
+            ]
+        }
+    })
 
+    if (user && await bcrypt.compare(password, user.password)) {
+        return true
+    }
+
+    return false
+}
+
+export const deleteUser = async (id: number) => {
+    await prisma.user.delete({ where: { id } })
 }
