0ff83c9dcf24caee30b290bbb56b4cfa46968068
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-03-05 15:27:37 -0300

    Feature: Add authService

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,16 @@
 "use client";
  
+import { createUser } from "@/server/services/authService";
 import { useSession } from "next-auth/react";
 
 
 
 export default () => {
     const user = useSession()
+
+    const handleCreateUser = () => {
+        createUser("Teste", "gS0yT@example.com", "123456789", "sector", "admin", "123456789", "buyer", "11999999999")
+    }
 
     return (
         <>
@@ -14,6 +19,8 @@ export default () => {
             <br />
             <hr />
             <pre>{JSON.stringify(user.data)}</pre>
+
+            <button onClick={handleCreateUser}>Create User</button>
         </>
 
     )
--- src/server/services/authService.ts
+++ src/server/services/authService.ts
@@ -1,9 +1,24 @@
+"use server"
+
 import { PrismaClient } from '@prisma/client'
 import { User } from '@/types/Auth'
 
 const prisma = new PrismaClient()
 
-export const createUser = async (data: Omit<User, 'id'>) => {
+export const createUser = async (
+    name: string,
+    email: string,
+    cpf: string,
+    sector: string,
+    role: string,
+    password: string,
+    accountType: "buyer" | "seller",
+    phoneNumber?: string,
+) => {
+    if (await prisma.user.findUnique({ where: { email } })) return;
 
+    await prisma.user.create({
+        data: { name, email, cpf, sector, role, accountType, password, phoneNumber }
+    })
 
 }
