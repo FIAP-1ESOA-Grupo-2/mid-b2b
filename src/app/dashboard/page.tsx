c3bef2184403c7acaa3db1c0fec18c3cb8174f72
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-03-04 21:49:20 -0300

    Hotfix: Auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,8 @@
- 
-import { authConfig } from "@/config/auth";
-import { getServerSession } from "next-auth/next" 
-import { signIn } from "next-auth/react";
-
 export default async function Page() {
-    const session = await getServerSession(authConfig)
-
-     signIn('credentials', { email: 'teste', password: 'teste' });
 
     return (
         <>
-            <pre>
-                {JSON.stringify(session, null, 2)}
-            </pre>
- 
+            DASHBOARD
         </>
-
     )
 }
--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,21 +1,5 @@
 import type { NextAuthOptions } from "next-auth"
 
-import Credentials from 'next-auth/providers/credentials';
-
 export const authConfig = {
-    providers: [
-        Credentials({
-            name: 'credentials',
-            credentials: {
-                username: { label: "Username", type: "text", placeholder: "jsmith" },
-                password: { label: "Password", type: "password" }
-            },
-            async authorize(credentials) {
-
-                console.log('Invalid credentials');
-                return { username: 'dd', password: 'd' };
-            },
-
-        }),
-    ],
+    providers: [],
 } satisfies NextAuthOptions
