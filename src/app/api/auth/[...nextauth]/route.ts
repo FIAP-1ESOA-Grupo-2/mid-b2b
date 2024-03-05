02885921da6b28e3e298be0aff13bc008e5b680c
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-03-05 14:55:24 -0300

    Fix: Conflict

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
+
+import { authConfig } from "@/config/auth";
 import NextAuth from "next-auth"
-import { authConfig } from "@/config/auth..config"
 
 const handler = NextAuth(authConfig)
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
