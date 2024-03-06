7cb748e2b5654b05454fd3f84cbe44d0753e36bb
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-03-05 21:32:28 -0300

    Add: next13-progressbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link"
+
 export default () => {
+
   return (
     <div>
       LANDING PAGE 2
+
+      <Link className="bg-teal-400 text-white " href='/dashboard'>
+        Login
+      </Link>
     </div>
   )
 }
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,6 +4,7 @@ import { store } from "@/redux/store"
 import { Provider } from "react-redux"
 import { SessionProvider } from "next-auth/react"
 import { ChakraProvider } from '@chakra-ui/react'
+import { Next13ProgressBar } from 'next13-progressbar';
 
 type Props = {
     children: React.ReactNode
@@ -13,6 +14,8 @@ export const Providers = ({ children }: Props) => {
     return (
         <Provider store={store}>
             <SessionProvider>
+                <Next13ProgressBar height="4px" color="#0a6cff" options={{ showSpinner: true }} showOnShallow />
+
                 <ChakraProvider>
                     {children}
                 </ChakraProvider>
