3b2960cc6e88441c0b8e6659a8ede35776c53d2f
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-03-10 12:56:52 -0300

    Create: middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest) {
+    const token = await getToken({ req: request });
+
+    if (!token && request.nextUrl.pathname !== "/auth/signin" && request.nextUrl.pathname !== "/auth/signup" && request.nextUrl.pathname !== "/auth/forgot-password") {
+        return NextResponse.redirect(new URL("/auth/signin", request.url));
+    }
+
+    if (token && (request.nextUrl.pathname === "/auth/signin" || request.nextUrl.pathname === "/auth/signup" || request.nextUrl.pathname === "/auth/forgot-password")) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+}
+
+export const config = { matcher: ["/dashboard/:path*", "/auth/:path*"] };
