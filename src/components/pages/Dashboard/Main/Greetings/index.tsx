7152944ffa5f3cba7eb0e7ec55b84cd194bf858b
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-03-11 20:57:28 -0300

    Change: Added loading on getting user info

--- src/components/pages/Dashboard/Main/Greetings/index.tsx
+++ src/components/pages/Dashboard/Main/Greetings/index.tsx
@@ -1,17 +1,20 @@
 import { useSession } from "next-auth/react"
-import { Avatar, SkeletonCircle } from "@chakra-ui/react";
+import { Avatar, Skeleton, SkeletonCircle } from "@chakra-ui/react";
 import { MdOutlineSearch } from "react-icons/md";
 import { IoFilterOutline } from "react-icons/io5";
 
 export const Greetings = () => {
     const auth = useSession()
+
     return (
         <section className="bg-white py-8 px-10 rounded-xl border shadow-sm border-slate-200">
             <section className="w-full flex items-center justify-between gap-3">
                 <div className="w-full">
                     <div className="flex items-center justify-between">
                         <div>
-                            <h1 className="font-bold text-xl mb-2">Oi {auth.data?.user.name}!</h1>
+                            <Skeleton isLoaded={auth.status == 'authenticated'} height={19} className="mb-2">
+                                <h1 className="font-bold text-xl">Olá, {auth.data?.user.name}!</h1>
+                            </Skeleton>
                             <p className="text-base mb-2">Pronto para fazer negócios?</p>
                         </div>
                         <div className="sm:hidden">
@@ -22,11 +25,11 @@ export const Greetings = () => {
                     </div>
                     <div className="flex justify-between items-center">
                         <div className="flex items-center p-3 border-[1px] rounded-xl w-full mr-2">
-                            <MdOutlineSearch size={24} color="#71717A"/>
-                            <input placeholder="O que está procurando?" type="text" className="w-full outline-none bg-transparent"/>
+                            <MdOutlineSearch size={24} color="#71717A" />
+                            <input placeholder="O que está procurando?" type="text" className="w-full outline-none bg-transparent" />
                         </div>
                         <div className="sm:hidden">
-                            <IoFilterOutline color="#007AB5" size={24}/>
+                            <IoFilterOutline color="#007AB5" size={24} />
                         </div>
                     </div>
                 </div>
