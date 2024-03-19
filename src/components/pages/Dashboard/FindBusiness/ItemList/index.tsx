3787565816d40644dc9fd507205024e7a7f1afbf
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-03-19 20:41:10 -0300

    Feature: MeetingSuggestions

--- src/components/pages/Dashboard/FindBusiness/ItemList/index.tsx
+++
@@ -1,8 +0,0 @@
-export const ItemList = () => {
-    return (
-        <div className="flex items-center gap-1 flex-wrap">
-            <p className="text-sm text-gray-500">Inovação e Avanços Tecnológicos, Inovações na Saúde, Varejo...</p>
-            <button className="text-mainblue text-base hover:text-mainbluehover hover:duration-100 hover:ease-in-out">Ver mais</button>
-        </div>
-    );
-}
--- src/server/services/meetingService.ts
+++ src/server/services/meetingService.ts
@@ -4,6 +4,7 @@ import { UserAccountType } from "@/types/Auth"
 import { PrismaClient } from "@prisma/client"
 import moment from 'moment';
 import 'moment/locale/pt-br'
+
 const prisma = new PrismaClient()
 
 export const generateMeetings = async (userId: number, userAccountType: UserAccountType) => {
@@ -16,11 +17,14 @@ export const generateMeetings = async (userId: number, userAccountType: UserAcco
 
     const userInterestsIds = userInterests.map((interest) => interest.interest.id)
 
+    const meetingInterestsExclude = await prisma.meeting.findMany({ where: { OR: [{ from_user_id: userId }, { to_user_id: userId }] }, select: { from_user_id: true, to_user_id: true } })
+    const userIdsExlcude = meetingInterestsExclude.map((meeting) => meeting.from_user_id === userId ? meeting.to_user_id : meeting.from_user_id)
+
     const interestsAllUsers = await prisma.userInterest.findMany({
         where: {
             user: {
                 id: {
-                    not: userId
+                    notIn: [...userIdsExlcude, userId]
                 },
                 accountType: {
                     not: userAccountType
@@ -35,7 +39,7 @@ export const generateMeetings = async (userId: number, userAccountType: UserAcco
 
     let meetingSuggestions: { userId: number, interests: string[], matchups: number }[] = []
 
-    const allMeetingSuggestions = await prisma.meetingSuggestion.findMany({ select: { from_user_id: true, to_user_id: true } })
+    const allMeetingSuggestions = await prisma.meetingSuggestion.findMany({ select: { from_user_id: true, to_user_id: true, refused: true } })
 
     userInterestsIds.forEach((interestId) => {
         const matchupExists = interestsAllUsers.find((user) => user.interest.id === interestId)
@@ -62,8 +66,7 @@ export const generateMeetings = async (userId: number, userAccountType: UserAcco
 
     const meetings = meetingSuggestions.sort(function (a, b) {
         return b.matchups - a.matchups
-    }).slice(0, 20 - allMeetingSuggestions.filter((suggestion) => suggestion.from_user_id === userId || suggestion.to_user_id === userId).length)
-
+    }).slice(0, 20 - allMeetingSuggestions.filter((suggestion) => (suggestion.from_user_id === userId || suggestion.to_user_id === userId) && !suggestion.refused).length)
 
     await prisma.meetingSuggestion.createMany({
         data: meetings.map((meeting) => ({
@@ -75,11 +78,53 @@ export const generateMeetings = async (userId: number, userAccountType: UserAcco
         })),
     })
 
-    const newMeetingsSuggestions = await prisma.meetingSuggestion.findMany({ where: { OR: [{ from_user_id: userId }, { to_user_id: userId }] } })
+    const newMeetingsSuggestions = await prisma.meetingSuggestion.findMany({ where: { OR: [{ from_user_id: userId }, { to_user_id: userId }], refused: false } })
     newMeetingsSuggestions.forEach((meeting) => {
         // @ts-ignore
         meeting.createdAt = moment(meeting.createdAt).calendar()
     })
 
     return newMeetingsSuggestions;
+}
+
+export const getMeetingSuggestion = async (id: number) => {
+    const meetingSuggestion = await prisma.meetingSuggestion.findFirst({ where: { id, refused: false } })
+    return meetingSuggestion
+}
+
+export const acceptMeetingSuggestion = async (id: number, userId: number) => {
+    if (await prisma.meetingSuggestion.findFirst({ where: { id, from_user_id: userId } })) {
+        await prisma.meetingSuggestion.update({ where: { id, from_user_id: userId }, data: { from_user_accepted: true } })
+    }
+
+    if (await prisma.meetingSuggestion.findFirst({ where: { id, to_user_id: userId } })) {
+        await prisma.meetingSuggestion.update({ where: { id, to_user_id: userId }, data: { to_user_accepted: true } })
+    }
+
+    const meetingSuggestion = await prisma.meetingSuggestion.findFirst({ where: { id, from_user_accepted: true, to_user_accepted: true } })
+    if (meetingSuggestion) {
+        // Delete meeting suggestion
+        await prisma.meetingSuggestion.delete({ where: { id: meetingSuggestion.id } })
+
+        // Create a meeting
+        await prisma.meeting.create({
+            data: {
+                matchups: meetingSuggestion.matchups,
+                interests: meetingSuggestion.interests,
+                from_user_id: meetingSuggestion.from_user_id,
+                to_user_id: meetingSuggestion.to_user_id,
+                createdAt: moment().toDate()
+            }
+        })
+
+        return { action: 'new_meeting' }
+    }
+
+    return { action: 'accepted' }
+}
+
+export const rejectMeetingSuggestion = async (id: number) => {
+    if (await prisma.meetingSuggestion.findFirst({ where: { id } })) {
+        await prisma.meetingSuggestion.update({ where: { id }, data: { refused: true } })
+    }
 }
